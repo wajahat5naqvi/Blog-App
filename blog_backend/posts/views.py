5e5d3fd83fc19527600b87e8a74e011b5709d5be
Syed1Wajahat <naqviwajahat@gmail.com>
2025-10-01 08:51:08 +0500

    minor change

--- blog_backend/posts/views.py
+++ blog_backend/posts/views.py
@@ -20,7 +20,6 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
 
 class PostViewSet(viewsets.ModelViewSet):
     """ViewSet for CRUD operations on posts"""
-    print("TEST")
     queryset = Post.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
